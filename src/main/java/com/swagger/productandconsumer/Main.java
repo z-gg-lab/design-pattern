d6dea5b9f5d7a8bec3c3c81dbaf57ccccc67a04b
朱炜 <wei.zhu@zkh360.com>
2017-12-25 14:31:04 +0800

    BlockingQueue的生产者消费者模式

---
+++ src/main/java/com/swagger/productandconsumer/Main.java
@@ -0,0 +1,26 @@
+package com.swagger.productandconsumer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * @author zhuTail
+ */
+public class Main {
+    public static void main(String[] args) {
+        //新建一个阻塞队列，队列长度是5
+        BlockingQueue<String> queue = new LinkedBlockingDeque<String>(5);
+//        BlockingQueue<String> queue2 = new ArrayBlockingQueue<String>(5);
+        Consumer consumer = new Consumer(queue);
+        Product product = new Product(queue);
+
+        for(int i = 0;i<3;i++){
+            new Thread(product,"product"+i).start();
+        }
+
+        for (int i = 0;i<5;i++){
+            new Thread(consumer,"consumer").start();
+        }
+    }
+}
---
+++ src/main/java/com/swagger/productandconsumer/Product.java
@@ -0,0 +1,28 @@
+package com.swagger.productandconsumer;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author zhuTail
+ */
+public class Product implements Runnable{
+
+    BlockingQueue<String> blockingQueue;
+
+    public Product(BlockingQueue<String> blockingQueue){
+        this.blockingQueue = blockingQueue;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName()+"开始生产...");
+            String temp =  Thread.currentThread().getName()+"：生产线程";
+            //向队列中放数据，如果队列是满的话，会阻塞当前线程
+            blockingQueue.put(temp);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
