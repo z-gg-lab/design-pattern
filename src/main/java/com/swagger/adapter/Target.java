ec7317dbe1fd23d4a241ad84f7a7bb5c4ed47d67
zhuTail <1015319800@qq.com>
2018-11-18 00:47:31 +0800

    漫漫长夜，无心睡眠~

--- src/main/java/com/swagger/adapter/Target.java
+++ src/main/java/com/swagger/adapter/Target.java
@@ -2,19 +2,19 @@ package com.swagger.adapter;
 
 /**
  * 目标角色：这就是所期待得到的接口。注意：由于这里讨论的是类适配器模式，因此目标不可以是类
- * @author Administrator
  *
+ * @author Administrator
  */
 public interface Target {
-	
-	/**
-	 * 这是源类Adaptee也有的方法
-	 */
-	public void sampleOperation1();
 
-	/**
-	 * 这是源类Adaptee没有的方法
-	 */
-	public void sampleOperation2();
+    /**
+     * 这是源类Adaptee也有的方法
+     */
+    void sampleOperation1();
+
+    /**
+     * 这是源类Adaptee没有的方法
+     */
+    void sampleOperation2();
 
 }
