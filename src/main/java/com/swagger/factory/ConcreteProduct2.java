0e83c8f93233d11fc404cde4ed13debbbe50a1ca
003364 <zhuwei@fcbox.com>
2020-07-24 14:45:15 +0800

    工厂模式demo优化

--- src/main/java/com/swagger/factory/ConcreteProduct2.java
+++ src/main/java/com/swagger/factory/ConcreteProduct2.java
@@ -7,6 +7,6 @@ public class ConcreteProduct2 extends Product {
 
     @Override
     public void method2() {
-        System.out.println("ConcreteProduct1 执行了 method2");
+        System.out.println("ConcreteProduct2 执行了 method2");
     }
 }
